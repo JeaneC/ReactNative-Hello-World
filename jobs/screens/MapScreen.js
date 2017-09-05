bf768dfda396bafa69e2ea5eb41b9a2d50f3927b
Jeane Carlos <volbohel@gmail.com>
2017-09-04 21:42:00 -0400

    Finished MapScreen

--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -1,20 +1,73 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
+import { MapView } from 'expo';
+import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
+
+import * as actions from '../actions';
 
 class MapScreen extends Component {
+  state = {
+    mapLoaded: false,
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09
+    }
+  }
+
+  componentDidMount(){
+    this.setState({ mapLoaded: true});
+  }
+
+  onRegionChangeComplete = (region) => {
+    this.setState({ region });
+  }
+
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region, () => {
+      this.props.navigation.navigate('deck')
+    });
+  }
+
   render() {
+    //This is android fixing code
+    if (!this.state.mapLoaded) {
+      return (
+        <View style= {{ flex: 1, justifyContent: 'center'}}>
+          <ActivityIndicator size ="large" />
+        </View>
+      )
+    }
+
     return (
-      <View>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
+      <View style={{ flex: 1}}>
+        <MapView
+          region={this.state.region}
+          style={{ flex: 1}}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        />
+        <View style ={styles.buttonContainer}>
+          <Button
+            large
+            title="Search This Area"
+            backgroundColor="#009688"
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 }
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0
+  }
+}
 
-export default MapScreen;
+export default connect(null, actions)(MapScreen);
